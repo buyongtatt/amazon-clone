454cc0f44a0a490065022ad1765f45f64ec3987a
021120 <yongtatt981213@hotmail.com>
2021-02-08 13:48:01 +0800

    Starting up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBD4wHP6oXV-c1Oc8H4JJeNktjA7zf0oy8",
+    authDomain: "cl-63edc.firebaseapp.com",
+    projectId: "cl-63edc",
+    storageBucket: "cl-63edc.appspot.com",
+    messagingSenderId: "427091549115",
+    appId: "1:427091549115:web:97e412319bdd6a21f6829e",
+    measurementId: "G-BP287BVJWZ"
+  };
