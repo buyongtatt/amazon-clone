0b7e34e3887af8f410383d2b6ad7a1246ae5171e
021120 <yongtatt981213@hotmail.com>
2021-02-12 15:16:33 +0800

    Checkout product

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -6,12 +6,20 @@ function CheckoutProduct({ id, image, title, price, rating }) {
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="" />
 
-      <div className="checkProduct__info">
+      <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
           <small>$</small>
           <strong>{price}</strong>
         </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map(() => (
+              <p>⭐</p>
+            ))}
+        </div>
+        <button>Remove from Basket</button>
       </div>
     </div>
   );
