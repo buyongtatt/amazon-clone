52f4e13e6eb101b798f1bb9c16147a625d111bf4
021120 <yongtatt981213@hotmail.com>
2021-02-14 11:01:12 +0800

    Payment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { auth } from "./firebase";
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
+import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
 
 function App() {
@@ -31,6 +32,10 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/payment">
+            <Header />
+            <Payment />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
